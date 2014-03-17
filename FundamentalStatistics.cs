926ec361fc92a9c3ebb53445f1f7df2f4dff9aa7
YuHori <yubert2014@gmail.com>
2014-03-17 13:31:17 +0900

    FundamentalStatistics
    基本統計

---
+++ FundamentalStatistics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatCSharp
+{
+    class FundamentalStatistics
+    {
+        /// <summary>
+        /// 平均値
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static double Mean(IEnumerable<double> data)
+        {
+            // 平均を計算
+            return (double)data.Sum() / data.Count();
+        }
+
+
+        /// <summary>
+        /// 標本分散
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static double Variance(IEnumerable<double> data)
+        {
+            if (data.Count() < 2)
+            {
+                throw new Exception("サンプルサイズが不足しています");
+            }
+
+            // 平均を計算
+            double averageValue = FundamentalStatistics.Mean(data);
+
+            // 分散を計算
+            return data.Select(num => Math.Pow(averageValue - num, 2)).Sum() / (data.Count() - 1);
+        }
+    }
+}
